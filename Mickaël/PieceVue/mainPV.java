b4f1ebb0bfa8066d8e11ffbdfccb94d420ef6fda
Albarell <Albarell>
2020-10-09 17:23:02 +0200

    pieceVue final

---
+++ Mickaël/PieceVue/mainPV.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class mainPV
+{
+	public static void main (String[] args)
+	{
+		JFrame fenetre = new JFrame("PV");
+		PieceVue pv = new PieceVue();
+		EventPV mouse;
+
+		fenetre.add(pv.getPanel(),BorderLayout.CENTER);
+		mouse = new EventPV(pv,fenetre);
+		fenetre.addMouseListener(mouse);
+
+		fenetre.setSize(500, 500);
+        fenetre.setLocation(100, 100);
+
+		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fenetre.setVisible(true);
+	}
+}
